833ed047b57e8d0b369b1592effb18b2b9b74b4b
Vlad Potra <potravlad@gmail.com>
2019-05-10 15:28:57 +0300

    Small routing example working, looks like crap, I used m-ui but idk why it looks like SHIT that damn nav-bar! FUCK IT, IT WORKS!

--- frontend/bookstore/src/app/app-routing.module.ts
+++ frontend/bookstore/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BooksComponent } from './books/books.component';
+import { HomeComponent } from './home/home.component';
+import { ClientsComponent } from './clients/clients.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent }, 
+  { path: 'books', component: BooksComponent }, 
+  { path: 'clients', component: ClientsComponent }, 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/bookstore/src/app/app.module.ts
+++ frontend/bookstore/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule, MatToolbarModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { NavComponent } from './nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { BooksComponent } from './books/books.component';
+import { ClientsComponent } from './clients/clients.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    BooksComponent,
+    ClientsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    BrowserAnimationsModule,
+    MatButtonModule, 
+    MatCheckboxModule, LayoutModule, MatToolbarModule, MatSidenavModule, MatIconModule, MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
